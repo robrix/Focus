cdf494ac726f0407d57f53031cee589bc64ccce9
Rob Rix <rob@monochromeindustries.com>
2011-04-13 03:28:39 -0400

    Prints stack traces on assertion failures.

--- Core/FAssertions.c
+++ Core/FAssertions.c
@@ -4,11 +4,24 @@
 
 #include "FAssertions.h"
 
+#include <execinfo.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 void FAssertionFailure(const char *file, unsigned int line, const char *function, const char *condition, const char *message) {
 	printf("%s:%u: in %s error: %s %s.\n", file, line, function, condition, message);
 	fflush(stdout);
+	
+	void *callstack[128];
+	size_t i, frames = backtrace(callstack, 128);
+	printf("\nStack trace (%lu frames):\n", frames);
+	char **strs = backtrace_symbols(callstack, frames);
+	for (i = 0; i < frames; ++i) {
+	    printf("%s\n", strs[i]);
+	}
+	free(strs);
+	
+	fflush(stdout);
+	
 	abort();
 }
